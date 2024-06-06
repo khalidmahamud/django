8e67c5ca928d1c671afd8625ded65ee5836bde10
khalidmahamud <22-46601-1@student.aiub.edu>
2024-06-06 23:06:15 +0600

    working on urls and viewa

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,36 +1,38 @@
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.shortcuts import render
+
+monthly_challenges = {
+    'january': "Eat no vegetables for the entire month!",
+    'february': "Walk for at least 20 minutes every day!",
+    'march': "Read one book this month!",
+    'april': "Learn a new skill!",
+    'may': "Exercise for at least 30 minutes every day!",
+    'june': "Drink 8 glasses of water every day!",
+    'july': "Meditate for 10 minutes every day!",
+    'august': "Write in a journal every day!",
+    'september': "Cook a new recipe every week!",
+    'october': "Limit screen time to 2 hours a day!",
+    'november': "Practice gratitude every day!",
+    'december': "Volunteer for a good cause!"
+}
 
 # Create your views here.
 
 
 def monthly_challenge(request, month):
-    challenge = None
-    if month == 'january':
-        challenge = "Eat no vegetables for the entire month!"
-    elif month == 'february':
-        challenge = "Walk for at least 20 minutes every day!"
-    elif month == 'march':
-        challenge = "Read one book this month!"
-    elif month == 'april':
-        challenge = "Learn a new skill!"
-    elif month == 'may':
-        challenge = "Exercise for at least 30 minutes every day!"
-    elif month == 'june':
-        challenge = "Drink 8 glasses of water every day!"
-    elif month == 'july':
-        challenge = "Meditate for 10 minutes every day!"
-    elif month == 'august':
-        challenge = "Write in a journal every day!"
-    elif month == 'september':
-        challenge = "Cook a new recipe every week!"
-    elif month == 'october':
-        challenge = "Limit screen time to 2 hours a day!"
-    elif month == 'november':
-        challenge = "Practice gratitude every day!"
-    elif month == 'december':
-        challenge = "Volunteer for a good cause!"
-    else:
+    try:
+        challenge = monthly_challenges[month]
+    except:
         return HttpResponseNotFound("This month is not supported!")
 
     return HttpResponse(challenge)
+
+
+def monthly_challenge_number(request, month):
+    months = list(monthly_challenges.keys())
+    if month > len(months):
+        return HttpResponseNotFound("Invalid month!")
+
+    redirect_month = months[month - 1]
+
+    return HttpResponseRedirect(f"/challenges/{redirect_month}")
