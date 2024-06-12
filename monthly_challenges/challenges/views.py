d8f7ba982d2a60627a8d20ead68ae2076f03e060
khalidmahamud <22-46601-1@student.aiub.edu>
2024-06-12 11:15:40 +0600

    working with tags in DTL

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -22,16 +22,11 @@ monthly_challenges = {
 
 
 def index(request):
-    list_items = ""
     months = list(monthly_challenges.keys())
 
-    for month in months:
-        month_path = reverse("month-challenge", args=[month])
-        list_items += f"<li><a href=\"{month_path}\">{month.capitalize()}</a></li>"
-
-    response_data = f"<ul>{list_items}</ul>"
-
-    return HttpResponse(response_data)
+    return render(request, "challenges/index.html", {
+        "months": months
+    })
 
 
 def monthly_challenge(request, month):
