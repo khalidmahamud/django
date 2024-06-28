2ebd2f3f8c82910fd02be6c089937407d0559dcd
khalidmahamud <22-46601-1@student.aiub.edu>
2024-06-28 16:34:57 +0600

    updated data in database

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -1,8 +1,16 @@
 from django.db import models
+from django.core.validators import MinLengthValidator, MaxLengthValidator
 
 # Create your models here.
 
 
 class Book(models.Model):
     title = models.CharField(max_length=50)
-    rating = models.IntegerField()
+    rating = models.IntegerField(
+        validators=[MinLengthValidator(1), MaxLengthValidator(5)]
+    )
+    author = models.CharField(max_length=100, null=True)
+    is_best_selling = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.title} by {self.author} ({self.rating}) {'Best Selling' if self.is_best_selling else 'Not Best Selling'}"
