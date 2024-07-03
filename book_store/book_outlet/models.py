fef80ee5908031c0897a1a61ffefcf623a962f42
khalidmahamud <22-46601-1@student.aiub.edu>
2024-07-03 23:09:46 +0600

    worked with django admin

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.core.validators import MinLengthValidator, MaxLengthValidator
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.urls import reverse
 from django.utils.text import slugify
 
@@ -9,7 +9,7 @@ from django.utils.text import slugify
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(
-        validators=[MinLengthValidator(1), MaxLengthValidator(5)]
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
     )
     author = models.CharField(max_length=100, null=True)
     is_best_selling = models.BooleanField(default=False)
@@ -17,10 +17,6 @@ class Book(models.Model):
 
     def get_absolute_url(self):
         return reverse("book-detail", args=[self.id, self.slug])
-
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)
-        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.title} by {self.author} ({self.rating}) {'Best Selling' if self.is_best_selling else 'Not Best Selling'}"
