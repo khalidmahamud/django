817635598363344ba358206e23c2c804ab5e6082
khalidmahamud <22-46601-1@student.aiub.edu>
2024-07-07 01:34:06 +0600

    worked with many-to-many relation

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -5,15 +5,25 @@ from django.utils.text import slugify
 
 # Create your models here.
 
+class Country(models.Model):
+    name = models.CharField(max_length=80)
+    code = models.CharField(max_length=2)
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = "Countries"
+
 
 class Address(models.Model):
     street = models.CharField(max_length=80)
     postal_code = models.CharField(max_length=5)
     city = models.CharField(max_length=50)
-    
+
     def __str__(self):
         return f"{self.street}, {self.postal_code}, {self.city}"
-    
+
     class Meta:
         verbose_name_plural = "Address Entries"
 
@@ -38,7 +48,8 @@ class Book(models.Model):
     author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
     is_best_selling = models.BooleanField(default=False)
     slug = models.SlugField(default="", null=False, db_index=True)
-
+    published_countries = models.ManyToManyField(Country)
+    
     def get_absolute_url(self):
         return reverse("book-detail", args=[self.id, self.slug])
 
