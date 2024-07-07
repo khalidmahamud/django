5f2abc06b6e5fa5c602880355673d038a29473fb
khalidmahamud <22-46601-1@student.aiub.edu>
2024-07-08 01:06:54 +0600

    completed database integration

--- my_blog/blog/admin.py
+++ my_blog/blog/admin.py
@@ -4,9 +4,12 @@ from .models import Post, Author, Tag
 
 # Register your models here.
 
+class PostAdmin(admin.ModelAdmin):
+    list_filter = ("author", "tags", "date",)
+    list_display = ("title", "author", "date",)
+    prepopulated_fields = {"slug": ("title",)}
 
 
-
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
 admin.site.register(Author)
 admin.site.register(Tag)
--- my_blog/blog/views.py
+++ my_blog/blog/views.py
@@ -1,78 +1,42 @@
 from datetime import date
 
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
+from .models import Post
 
-all_posts = [
-    {
-        "slug": "hike-in-the-mountain",
-        "image": "mountains.jpg",
-        "author": "Khalid",
-        "date": date(2024, 6, 21),
-        "title": "Mountain Hiking",
-        "excerpt": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum magnam quos consectetur at velit dolorem.",
-        "content": 
-            """Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae amet, tenetur dolorem soluta possimus eaque aspernatur laboriosam! Ipsam assumenda consequuntur dicta cum magni doloremque vitae quos. Cumque officiis sapiente reiciendis?
-            
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus repellendus deserunt, facilis perferendis ducimus aliquid ipsam atque cupiditate! Autem molestias ducimus a iste temporibus minima quisquam commodi exercitationem. Dolore, quaerat!
-            
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo sequi, officia modi quod, dolor fuga soluta sunt consequatur facere obcaecati voluptas ratione atque iusto vero voluptate dolores, voluptatibus harum minima.
-        """,
-    },
-    {
-        "slug": "programming-is-fun",
-        "image": "coding.jpg",
-        "author": "Khalid",
-        "date": date(2024, 5, 10),
-        "title": "Programming is Great!",
-        "excerpt": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum magnam quos consectetur at velit dolorem.",
-        "content":
-            """Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae amet, tenetur dolorem soluta possimus eaque aspernatur laboriosam! Ipsam assumenda consequuntur dicta cum magni doloremque vitae quos. Cumque officiis sapiente reiciendis?
-            
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus repellendus deserunt, facilis perferendis ducimus aliquid ipsam atque cupiditate! Autem molestias ducimus a iste temporibus minima quisquam commodi exercitationem. Dolore, quaerat!
-            
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo sequi, officia modi quod, dolor fuga soluta sunt consequatur facere obcaecati voluptas ratione atque iusto vero voluptate dolores, voluptatibus harum minima.
-        """,
-    },
-    {
-        "slug": "into-the-woods",
-        "image": "woods.jpg",
-        "author": "Khalid",
-        "date": date(2024, 6, 21),
-        "title": "Nature At Its Best",
-        "excerpt": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum magnam quos consectetur at velit dolorem.",
-        "content": 
-            """Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae amet, tenetur dolorem soluta possimus eaque aspernatur laboriosam! Ipsam assumenda consequuntur dicta cum magni doloremque vitae quos. Cumque officiis sapiente reiciendis?
-            
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus repellendus deserunt, facilis perferendis ducimus aliquid ipsam atque cupiditate! Autem molestias ducimus a iste temporibus minima quisquam commodi exercitationem. Dolore, quaerat!
-            
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo sequi, officia modi quod, dolor fuga soluta sunt consequatur facere obcaecati voluptas ratione atque iusto vero voluptate dolores, voluptatibus harum minima.
-        """,
-    },
-]
-
-def get_date(post):
-    return post['date']
+all_posts = Post.objects.all().order_by("-date")
 
 # Create your views here.
 
 
 def home_page(request):
-    sorted_posts = sorted(all_posts, key=get_date)
-    latest_posts = sorted_posts[-3:]
-    return render(request, "blog/home.html", {
-        "posts": latest_posts,
-    })
+    latest_posts = all_posts[:3]
+    return render(
+        request,
+        "blog/home.html",
+        {
+            "posts": latest_posts,
+        },
+    )
 
 
 def posts_page(request):
-    return render(request, "blog/posts.html", {
-        "all_posts": all_posts,
-    })
+    return render(
+        request,
+        "blog/posts.html",
+        {
+            "all_posts": all_posts,
+        },
+    )
 
 
 def single_post_page(request, slug):
-    identified_post = next(post for post in all_posts if post['slug'] == slug)
-    return render(request, "blog/single_post.html", {
-        "post": identified_post,
-    })
+    identified_post = get_object_or_404(all_posts, slug=slug)
+    return render(
+        request,
+        "blog/single_post.html",
+        {
+            "post": identified_post,
+            "post_tags": identified_post.tags.all(),
+        },
+    )
