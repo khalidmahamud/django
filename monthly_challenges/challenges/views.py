2a6dbd86990d2f08c37ae81360685f28d18f1b5d
khalidmahamud <22-46601-1@student.aiub.edu>
2024-07-14 22:16:34 +0600

    practisingg form

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,21 +1,27 @@
-from django.http import HttpResponse, Http404, HttpResponseNotFound, HttpResponseRedirect
+from django.http import (
+    HttpResponse,
+    Http404,
+    HttpResponseNotFound,
+    HttpResponseRedirect,
+)
 from django.shortcuts import render
 from django.urls import reverse
+
 # from django.template.loader import render_to_string
 
 monthly_challenges = {
-    'january': "Eat no vegetables for the entire month!",
-    'february': "Walk for at least 20 minutes every day!",
-    'march': "Read one book this month!",
-    'april': "Learn a new skill!",
-    'may': "Exercise for at least 30 minutes every day!",
-    'june': "Drink 8 glasses of water every day!",
-    'july': "Meditate for 10 minutes every day!",
-    'august': "Write in a journal every day!",
-    'september': "Cook a new recipe every week!",
-    'october': "Limit screen time to 2 hours a day!",
-    'november': "Practice gratitude every day!",
-    'december': None
+    "january": "Eat no vegetables for the entire month!",
+    "february": "Walk for at least 20 minutes every day!",
+    "march": "Read one book this month!",
+    "april": "Learn a new skill!",
+    "may": "Exercise for at least 30 minutes every day!",
+    "june": "Drink 8 glasses of water every day!",
+    "july": "Meditate for 10 minutes every day!",
+    "august": "Write in a journal every day!",
+    "september": "Cook a new recipe every week!",
+    "october": "Limit screen time to 2 hours a day!",
+    "november": "Practice gratitude every day!",
+    "december": None,
 }
 
 # Create your views here.
@@ -24,18 +30,17 @@ monthly_challenges = {
 def index(request):
     months = list(monthly_challenges.keys())
 
-    return render(request, "challenges/index.html", {
-        "months": months
-    })
+    return render(request, "challenges/index.html", {"months": months})
 
 
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        return render(request, "./challenges/challenge.html", {
-            "month_name": month,
-            "text": challenge_text
-        })
+        return render(
+            request,
+            "./challenges/challenge.html",
+            {"month_name": month, "text": challenge_text},
+        )
     except:
         raise Http404()
 
