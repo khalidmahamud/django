f8918293756c13f8bedd6a8ec370db93e0161f6b
khalidmahamud <22-46601-1@student.aiub.edu>
2024-09-11 01:36:04 +0600

    working with ssession

--- feedback/feedback/settings.py
+++ feedback/feedback/settings.py
@@ -122,6 +122,8 @@ STATIC_URL = "static/"
 MEDIA_ROOT = BASE_DIR / "uploads"
 MEDIA_URL = "/user-media/"
 
+# SESSION_COOKIE_AGE = 120
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
