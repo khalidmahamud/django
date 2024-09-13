e562776fcad9a3965eb2250add1775b548fc3703
khalidmahamud <22-46601-1@student.aiub.edu>
2024-09-13 20:43:36 +0600

    added login

--- dcrm/dcrm/website/views.py
+++ dcrm/dcrm/website/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
@@ -6,11 +6,27 @@ from django.contrib import messages
 
 
 def home(request):
-    return render(request, "home.html", {})
+    # Check to see if logged in
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        # Authenticate
+        user = authenticate(
+            request, username=username, password=password
+        )
+        if user is not None:
+            login(request, user)
+            messages.success(request,"You Have Been Logged In!")
+            return redirect('home')
+        else:
+            messages.success(request, "There Was An Error Logging In, Please Try Again...")
+            return redirect('home')
+    else:
+        return render(request, "home.html", {})
 
 
-def login_user(request):
-    pass
+# def login_user(request):
+#     pass
 
 
 def logout_user(request):
